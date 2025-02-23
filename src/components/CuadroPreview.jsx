c0201c3f7775fc526ac79196f1b2b1e5abd9fab7
Agus Altamirano <agusalta002@gmail.com>
2025-02-22 23:15:43 -0300

    Update cruz de cerrar modal

--- src/components/CuadroPreview.jsx
+++ src/components/CuadroPreview.jsx
@@ -14,27 +14,29 @@ function CuadroPreview({ cuadro, onClose }) {
   }
 
   return (
-    <div className="preview" data-target={cuadro.id}>
-      <img
-        className="cuadro"
-        src={require("../img/" + cuadro.imagen)}
-        alt={cuadro.nombre}
-        title={cuadro.nombre}
-      />
-      <h3 title={cuadro.nombre}>{cuadro.nombre}</h3>
-      <p>
-        <strong>Medidas:</strong> {cuadro.medidas}
-      </p>
-      <p>
-        <strong>Precio:</strong> {cuadro.precio}{" "}
-        <span className="usd">USD</span>
-      </p>
-      <button className="agregar-al-carrito" onClick={handleRedireccion}>
-        Mas información
-      </button>
-      <button className="cerrar-modal" onClick={onClose}>
-        X
-      </button>
+    <div className="cuadro-imagen">
+      <div className="preview" data-target={cuadro.id}>
+        <img
+          className="cuadro"
+          src={require("../img/" + cuadro.imagen)}
+          alt={cuadro.nombre}
+          title={cuadro.nombre}
+        />
+        <h3 title={cuadro.nombre}>{cuadro.nombre}</h3>
+        <p>
+          <strong>Medidas:</strong> {cuadro.medidas}
+        </p>
+        <p>
+          <strong>Precio:</strong> {cuadro.precio}{" "}
+          <span className="usd">USD</span>
+        </p>
+        <button className="agregar-al-carrito" onClick={handleRedireccion}>
+          Mas información
+        </button>
+        <button className="cerrar-modal" onClick={onClose}>
+          <p>X</p>
+        </button>
+      </div>
     </div>
   );
 }
